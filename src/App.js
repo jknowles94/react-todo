891e6efc0ea65ca999fbf017408cb20fcf819a43
Jake Knowles <jakeyknowles@hotmail.co.uk>
2019-01-23 18:41:45 +0000

    Think of the layout of to do list with list of requirements

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todo from './containers/Todo/Todo';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <h1>Todo list</h1>
+        <ul>
+          <li>Ability to create new list items</li>
+          <li>Get a list of current todo items from placeholder API</li>
+          <li>Ability to set list item as completed</li>
+          <li>Ability to delete list items</li>
+          <li>Edit existing list items</li>
+          <li>Add animations to the different handlers</li>
+        </ul>
+        <Todo></Todo>
+      </React.Fragment>
     );
   }
 }
