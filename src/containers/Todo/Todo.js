0b9d76f9f2255c76e6cafdfc7d9bf3a49ca470f9
Jake Knowles <jakeyknowles@hotmail.co.uk>
2019-02-08 10:28:06 +0000

    add axios and use async await to get data

--- src/containers/Todo/Todo.js
+++ src/containers/Todo/Todo.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import './Todo.css';
 import TodoList from '../../components/TodoList/TodoList';
 import AddNewItem from '../../components/addNewItem/addNewItem';
+import axios from 'axios';
 
 class Todo extends Component {
+	state = {
+		todos: []
+	}
+	
+	async componentDidMount() {
+		const response = await axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10');
+		const data = await response.data;
+		this.setState({todos: data});
+	}
+
   render() {
     return (
     	<div className="todo_container">
     		<h1>Tasks</h1>
-    		<TodoList></TodoList>
+    		<TodoList todos={this.state.todos}></TodoList>
     		<AddNewItem></AddNewItem>
     	</div>
     );
